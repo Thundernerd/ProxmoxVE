c1e42361e1ed0bf385eabdc609fc59b38d6d8121
Christiaan Bloemendaal <github@tnrd.net>
2025-07-16 17:19:26 +0200

    Suwayomi

--- install/suwayomiserver-install.sh
+++ install/suwayomiserver-install.sh
@@ -19,13 +19,7 @@ msg_ok "Installed Dependencies"
 
 JAVA_VERSION=21 setup_java
 
-msg_info "Settting up Suwayomi-server"
-temp_file=$(mktemp)
-RELEASE=$(curl -fsSL https://api.github.com/repos/Suwayomi/Suwayomi-Server-preview/releases/latest | grep "tag_name" | awk '{print substr($2, 2, length($2)-3) }')
-curl -fsSL "https://github.com/Suwayomi/Suwayomi-Server-preview/releases/download/${RELEASE}/Suwayomi-Server-${RELEASE}-debian-all.deb" -o "$temp_file"
-$STD dpkg -i "$temp_file"
-echo "${RELEASE}" >/opt/Suwayomi-server_version.txt
-msg_ok "Done setting up Suwayomi-server"
+fetch_and_deploy_gh_release "Suwayomi-server" "Suwayomi/Suwayomi-Server-preview" "binary"
 
 msg_info "Creating Service"
 cat <<EOF >/etc/systemd/system/Suwayomi-server.service
@@ -34,7 +28,10 @@ Description=Suwayomi-server Service
 After=network.target
 
 [Service]
+User=root
+Type=simple
 ExecStart=/usr/bin/Suwayomi-server
+TimeoutStopSec=20
 Restart=always
 
 [Install]
